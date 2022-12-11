b028f0faf6c191bae4f6de3a32e7ed80af616a6f
Oreo-Monster <ewrong16@gmail.com>
2022-12-11 15:16:35 -0600

    added functions and doc strings

---
+++ CS375_Proj4_Code_Team_AT_CZ_EW_XM.py
@@ -0,0 +1,39 @@
+'''
+CS375 Project 4
+Aidan Tokarski, Cynthia Zafris,
+Eda Wright, Xavier Markowitz
+
+This program explores different edit distance algorithms
+and a spell spell checker. 
+
+Usage:
+#TODO add usage for each example
+
+
+'''
+
+
+
+def editDistance_rec(S,T):
+    '''
+    TODO add docstring with I/O
+    '''
+    pass
+
+
+def editDistance_iter(S,T):
+    '''
+    TODO add docstring with I/O
+    '''
+    pass
+
+
+def spellCheck(T, D):
+    '''
+    Input: A sting of text T to be spell checked. A dictionary D of correctly spelled words.
+
+    Output: For every word w in T that does not occour in D, 5 words in D with minimal edit distance.
+    These will be returned as a dictionary with the w as the key and the value a list of spelling suggestions.
+    '''
+    pass
+

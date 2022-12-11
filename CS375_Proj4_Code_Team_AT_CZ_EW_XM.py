5770d7b5f636048739f056b7bf46e4525548fd57
cezaf <117246506+cezaf@users.noreply.github.com>
2022-12-11 16:21:20 -0500

    added recursive and iterative methods

--- CS375_Proj4_Code_Team_AT_CZ_EW_XM.py
+++ CS375_Proj4_Code_Team_AT_CZ_EW_XM.py
@@ -18,14 +18,52 @@ def editDistance_rec(S,T):
     '''
     TODO add docstring with I/O
     '''
-    pass
+    if S == '':
+        return len(T) 
+    elif T == '':
+        return len(S)
+    elif S[len(S) - 1] == T[len(T) - 1]:
+        return editDistance_rec(S[:len(S) - 2],T[:len(T) - 2]) 
+    else:
+        return 1 + min( editDistance_rec(S[:len(S) - 2],T[:len(T) - 2]), 
+                        editDistance_rec(S[:len(S) - 2],T[:len(T) - 1]),
+                        editDistance_rec(S[:len(S) - 1],T[:len(T) - 2]) )
 
 
 def editDistance_iter(S,T):
     '''
     TODO add docstring with I/O
     '''
-    pass
+    m = len(S)
+    n = len(T)
+
+    grid = []
+    for i in range(m + 1): # rows: S, using m
+        grid.append([])
+        for j in range(n + 1): # columns: T, using n
+            grid[i].append(0)  
+
+    for i in range(1,m + 1): # base case for rows
+        grid[i][0] = i  
+    
+    for j in range(1,n + 1): # base case for columns
+        grid[0][j] = j
+
+    for i in range(1, m + 1): # all but the first row / first column
+        for j in range(1, n + 1): 
+            if S[i - 1] == T[j - 1]: # if specified letters in S and T are the same
+                grid[i][j] = grid[i - 1][j - 1] 
+            else: # if specified letters in S and T are not the same
+                grid[i][j] = 1 + min( 
+                                grid[i - 1][j - 1],
+                                grid[i - 1][j],
+                                grid[i][j - 1]
+                            ) 
+
+    for i in range(m + 1): # printing out grid
+        print(grid[i])  
+    
+    return grid[m][n]
 
 
 def spellCheck(T, D):

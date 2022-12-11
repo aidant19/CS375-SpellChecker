a7baa536c1071a77418be96304da8063d9a4901c
Oreo-Monster <ewrong16@gmail.com>
2022-12-11 16:16:09 -0600

    finished spellchecker

--- CS375_Proj4_Code_Team_AT_CZ_EW_XM.py
+++ CS375_Proj4_Code_Team_AT_CZ_EW_XM.py
@@ -35,5 +35,36 @@ def spellCheck(T, D):
     Output: For every word w in T that does not occour in D, 5 words in D with minimal edit distance.
     These will be returned as a dictionary with the w as the key and the value a list of spelling suggestions.
     '''
-    pass
+    out = {} #Output dictionary
+    words = T.lower().split() 
+    for word in words:
+        if word not in D:
+            out[word] = findSuggestions(word, D)
+    return out
+
+
+def findSuggestions(word, D):
+    '''
+    Input: A mispelled word, a dictionary of ccorrectly spelled words D
+
+    Output: Returns a list of 5 words from D with the closest edit distance to word. If there is a tie, the word
+    that appears in D first is valued closer to the word.
+    '''
+    out = [] #Output list
+    editDistances = [1e10, 1e10, 1e10, 1e10, 1e10] #keeps track of the 5 smallest edit distances so far
+    for d in D:
+        editDistance = editDistance_iter(d, word)
+        k = 5 #index where new word should be inserted
+        while k > 0 or editDistance < editDistances[k-1]:
+            k -= 1 #d belongs higher in the list
+        #Inserting d and edit distance into their lists
+        out.insert(k, d)
+        editDistance.insert(k, editDistance)
+        #Trimming list to sill be 5 long
+        out = out[:5]
+        editDistances = editDistance[:5]
+    return out
+
+
+
 
